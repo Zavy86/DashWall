b07c2385e2b70ce3d68b89c7f669450b22677a67
Manuel Zavatta <manuel.zavatta@gmail.com>
2019-03-04 16:59:15 +0100

    Sample configuration file

---
+++ config.sample.inc.php
@@ -0,0 +1,10 @@
+<?php
+ // directory
+ $configuration->path="/dashwall/";
+ // database parameters
+ $configuration->db_type="mysql";
+ $configuration->db_host="localhost";
+ $configuration->db_name="dashwall";
+ $configuration->db_user="developer";
+ $configuration->db_pass="developer";
+?>
