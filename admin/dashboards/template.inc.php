c3b7f3e5ae3e4529beccfeee928a8f93fb384936
Manuel Zavatta <manuel.zavatta@gmail.com>
2019-03-20 17:37:21 +0100

    Dashboard preview link

--- admin/dashboards/template.inc.php
+++ admin/dashboards/template.inc.php
@@ -17,6 +17,7 @@ if($dashboard_obj->id && in_array(SCRIPT,array("dashboard_view","dashboard_edit"
  $nav->addSubItem("Edit dashboard","admin.php?mod=".MODULE."&scr=dashboard_edit&idDashboard=".$dashboard_obj->id);
  $nav->addSubItem("Reorder tiles","admin.php?mod=".MODULE."&scr=submit&act=dashboard_tile_reorder&idDashboard=".$dashboard_obj->id);
  $nav->addSubItem("Add new tile","admin.php?mod=".MODULE."&scr=dashboard_view&act=tile_add&idDashboard=".$dashboard_obj->id);
+ $nav->addSubItem("Preview","index.php?dashboard=".$dashboard_obj->id,true,null,null,null,null,"_blank");
 }else{$nav->addItem("Add","admin.php?mod=".MODULE."&scr=dashboard_edit");}
 // renderize nav into bootstrap sections
 $bootstrap->addSection($nav->render(false,3));
