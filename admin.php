15e8396d8d51bd9cca9f6b21cbb2967f0d5757ed
Manuel Zavatta <manuel.zavatta@gmail.com>
2019-03-20 17:09:26 +0100

    Administration update

--- admin.php
+++ admin.php
@@ -6,7 +6,6 @@
  * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
  * @link    https://github.com/Zavy86/dashwall
  */
-
  // include functions
  require_once("functions.inc.php");
  // include bootstrap structures
@@ -21,34 +20,26 @@
  if($r_action){define(ACTION,$r_action);}else{define(ACTION,null);}
  // globals variables
  global $bootstrap;
-
-
- // template ------------
-
  // build bootstrap structure
  $bootstrap=new strBootstrap($APP->path);
-
-
- //
+ // build navbar
  $navbar=new strNavbar($APP->title);
  $navbar->addNav();
-
  $navbar->addElement("Administration","admin.php?mod=administration");
  $navbar->addElement("Dashboards","admin.php?mod=dashboards");
-
- //
+ // add navbar to bootstrap
  $bootstrap->addSection($navbar->render(3));
-
- // /template ----------
-
-
  // check and import script
  if(!is_dir($APP->dir."admin/".MODULE)){api_alert("Module \"".MODULE."\" was not found..","danger");api_redirect($APP->path."admin.php");}
  if(!file_exists($APP->dir."admin/".MODULE."/".SCRIPT.".inc.php")){api_alert("Script \"".SCRIPT."\" was not found in module \"".MODULE."\"..","danger");}
  else{require_once($APP->dir."admin/".MODULE."/".SCRIPT.".inc.php");}
-
-
- // renderize
+ // build footer grid
+ $footer_grid=new strGrid();
+ $footer_grid->addRow("footer");
+ $footer_grid->addCol(str_repeat(" ",6).api_tag("div","Copyright 2018-".date("Y")." &copy; <a href=\"https://github.com/Zavy86/dashwall\" target=\"_blank\">Dash|Wall</a> - All Rights Reserved","text-right")."\n","col-xs-12");
+ // add footer grid to bootstrap
+ $bootstrap->addSection(str_repeat(" ",3)."<hr>\n".$footer_grid->render(true,3));
+ // renderize bootstrap
  $bootstrap->render();
  // debug
  api_dump($APP,"Dash|Wall");
