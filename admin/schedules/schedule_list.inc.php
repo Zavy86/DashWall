8f0701b0e30ae3fcd439ce18e27a56890bca0dc3
Manuel Zavatta <manuel.zavatta@gmail.com>
2019-03-21 14:45:43 +0100

    Schedule list update and fixs

--- admin/schedules/schedule_list.inc.php
+++ admin/schedules/schedule_list.inc.php
@@ -15,8 +15,8 @@
  $table=new strTable("There is no schedule to show..");
  // add table headers
  $table->addHeader("&nbsp;");
- $table->addHeader("Schedule",null,"100%");
- $table->addHeader("Plugin","nowrap text-right");
+ $table->addHeader("Schedule","nowrap");
+ $table->addHeader("Plugin",null,"100%");
  $table->addHeader("Hours","nowrap text-right");
  $table->addHeader("Minutes","nowrap text-right");
  // get schedules
@@ -27,24 +27,28 @@
  foreach($schedules_array as $schedule_fobj){
   // check selected
   if($schedule_fobj->id==$_REQUEST['idSchedule']){$tr_class="info";}else{$tr_class=null;}
+  // make td plugin
+  $td_plugin=$schedule_fobj->plugin."(";
+  if(count($schedule_fobj->parameters_array)){$td_plugin.=stripslashes(json_encode($schedule_fobj->parameters_array));}
+  $td_plugin.=")";
   // add table datas
   $table->addRow($tr_class);
   $table->addRowFieldAction("admin.php?mod=schedules&scr=schedule_edit&idSchedule=".$schedule_fobj->id,api_icon("pencil","Edit this schedule"));
   $table->addRowField($schedule_fobj->title);
-  $table->addRowField(api_tag("samp",$schedule_fobj->plugin),"nowrap text-right");
-  $table->addRowField($schedule_fobj->hours,"nowrap text-right");
-  $table->addRowField($schedule_fobj->minutes,"nowrap text-right");
+  $table->addRowField(api_tag("samp",$td_plugin));
+  $table->addRowField($schedule_fobj->hours." ","nowrap text-right");
+  $table->addRowField($schedule_fobj->minutes." ","nowrap text-right");
  }
- // make alert
- $alert=api_tag("div","Sample crontab script: <samp>*  *  *  *  *  wget \"".$APP->url."cron.php\" -qO- | cat /dev/null</samp>","alert alert-info");
  // build grid
  $grid=new strGrid();
  // add grid row
  $grid->addRow();
- // renderize alert into grid
- $grid->addCol($alert,"col-xs-12");
- // add grid row
- $grid->addRow();
+ if(DEBUG){
+  // make alert
+  $alert=api_tag("div","Sample crontab script: <samp>*  *  *  *  *  wget \"".$APP->url."cron.php\" -qO- | cat /dev/null</samp>","alert alert-info");
+  // renderize alert into grid
+  $grid->addCol($alert,"col-xs-12");
+ }
  // renderize table into grid
  $grid->addCol($table->render(6),"col-xs-12");
  // renderize grid into bootstrap sections
