269355a91feb8557e0639a0adeb33a3589907743
Manuel Zavatta <manuel.zavatta@gmail.com>
2019-03-21 09:16:28 +0100

    Configuration updated

--- config.sample.inc.php
+++ config.sample.inc.php
@@ -7,4 +7,6 @@
  $configuration->db_name="dashwall";
  $configuration->db_user="username";
  $configuration->db_pass="password";
+ // authentication (md5 password)
+ $configuration->authentication="5f4dcc3b5aa765d61d8327deb882cf99";
 ?>
