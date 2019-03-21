3a466fbcd5e93c0748461c5a53d4ac72a564e894
Manuel Zavatta <manuel.zavatta@gmail.com>
2019-03-21 09:27:54 +0100

    Add label fix

--- admin/dashboards/template.inc.php
+++ admin/dashboards/template.inc.php
@@ -18,6 +18,6 @@ if($dashboard_obj->id && in_array(SCRIPT,array("dashboard_view","dashboard_edit"
  $nav->addSubItem("Reorder tiles","admin.php?mod=".MODULE."&scr=submit&act=dashboard_tile_reorder&idDashboard=".$dashboard_obj->id);
  $nav->addSubItem("Add new tile","admin.php?mod=".MODULE."&scr=dashboard_view&act=tile_add&idDashboard=".$dashboard_obj->id);
  $nav->addSubItem("Preview","index.php?dashboard=".$dashboard_obj->id,true,null,null,null,null,"_blank");
-}else{$nav->addItem("Add","admin.php?mod=".MODULE."&scr=dashboard_edit");}
+}else{$nav->addItem("Add new dashboard","admin.php?mod=".MODULE."&scr=dashboard_edit");}
 // renderize nav into bootstrap sections
 $bootstrap->addSection($nav->render(false,3));
